998b20d1c3bc6f0e8dbefa6c10bb8514e030b638
nerminaf01 <aoufnermine02@gmail.com>
2025-02-18 09:33:12 +0100

    Modification du programme pour afficher "Bienvenue le monde!"
    Ajout du fichier README
    
    Modification du fichier README pour refléter le nouveau message
    
    Renommage du fichier README en README.md
    
    Modification du programme de bienvenue
    
    Ajout du fichier .gitignore
    
    Ajout du programme de bienvenue
    
    mise a jour du fichier README
    
    Renommage du fichier README en README.md
    
    Message de validation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
+# Bienvenue\nProgramme C++ qui affiche "Bienvenue le monde!"
+=======
 # Bienvenue
 Programme C++ qui affiche "Bienvenue le monde !"
+>>>>>>> 0d0452a... mise a jour du fichier README
--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,7 +1,9 @@
-// Affiche un message de bienvenue
+// TODO Indiquer ce que fait le programme
 #include <iostream>
+
 int main()
 {
     std::cout << "Bienvenue le monde !" << std::endl;
+    std::cout << "Bienvenue dans le programme !\n";
     return 0;
 }
